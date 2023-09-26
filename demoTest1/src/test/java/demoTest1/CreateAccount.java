f2efb4ac858fe2e5192c0d282f1a457fc26b7270
eriksown <kierson.vigilla@gmail.com>
2023-09-25 22:55:31 -0400

    Revert "Added script for required fields validation"
    This reverts commit 547dabf32194a489ecb0eebe77b66a63b1b50f56.

--- demoTest1/src/test/java/demoTest1/CreateAccount.java
+++ demoTest1/src/test/java/demoTest1/CreateAccount.java
@@ -6,7 +6,6 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -21,7 +20,7 @@ public class CreateAccount extends Driver{
 	
 	@Test
 	public void accessWebsite() {
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 		
 		try {
 			driver.get("https://magento.softwaretestingboard.com/");
@@ -30,56 +29,27 @@ public class CreateAccount extends Driver{
 			System.out.println("The demo store website is not available");
 		}
 		
-	//Verify Home page loads successfully
+		//Verify Home page loads successfully
 		String homeTitle = driver.getTitle();
 		assertEquals(homeTitle, "Home Page");
 		
-	//Verify Create an Account link available and redirects user to create customer account
+		//Verify Create an Account link available and redirects user to create customer account
 		driver.findElement(By.linkText("Create an Account")).click();		
 		String newAccountTitle = driver.getTitle();
 		assertEquals(newAccountTitle, "Create New Customer Account");
 		
-		WebDriverWait waitPersonalInfoFields = new WebDriverWait(driver,Duration.ofSeconds(2));
+		WebDriverWait waitPersonalInfoFields = new WebDriverWait(driver,Duration.ofSeconds(3));
 		waitPersonalInfoFields.until(ExpectedConditions.visibilityOfElementLocated(By.id("form-validate")));
-		
-	//Verify validation is NOT present upon first load of Create Account	
-		WebDriverWait invRequiredFieldError = new WebDriverWait(driver,Duration.ofMillis(1));
-		invRequiredFieldError.until(ExpectedConditions.invisibilityOfElementLocated(By.id("firstname-error")));
-		System.out.println("First Name validation not yet available");
-		invRequiredFieldError.until(ExpectedConditions.invisibilityOfElementLocated(By.id("lastname-error")));
-		System.out.println("Last Name validation not yet available");
-		invRequiredFieldError.until(ExpectedConditions.invisibilityOfElementLocated(By.id("email_address-error")));
-		System.out.println("Email Address validation not yet available");
-		invRequiredFieldError.until(ExpectedConditions.invisibilityOfElementLocated(By.id("password-error")));
-		System.out.println("Password validation not yet available");
-		invRequiredFieldError.until(ExpectedConditions.invisibilityOfElementLocated(By.id("password-confirmation-error")));
-		System.out.println("Password Confirmation not yet available\n");
-		
-	//Verify validation for required fields
-		driver.findElement(By.xpath("//div[@class=\"primary\"]/button")).click();
-		WebDriverWait requiredFieldError = new WebDriverWait(driver,Duration.ofMillis(1));
-		requiredFieldError.until(ExpectedConditions.visibilityOfElementLocated(By.id("firstname-error")));
-		System.out.println("First Name validation available");
-		requiredFieldError.until(ExpectedConditions.visibilityOfElementLocated(By.id("lastname-error")));
-		System.out.println("Last Name validation available");
-		requiredFieldError.until(ExpectedConditions.visibilityOfElementLocated(By.id("email_address-error")));
-		System.out.println("Email Address validation available");
-		requiredFieldError.until(ExpectedConditions.visibilityOfElementLocated(By.id("password-error")));
-		System.out.println("Password validation available");
-		requiredFieldError.until(ExpectedConditions.visibilityOfElementLocated(By.id("password-confirmation-error")));
-		System.out.println("Password Confirmation available\n");
-		
-	//Fill up form
 		driver.findElement(By.id("firstname")).sendKeys("Apo");
 		driver.findElement(By.id("lastname")).sendKeys("Ambo");
 		driver.findElement(By.name("email")).sendKeys("kierson.vigilla@uap.asia");
 		
-	//Scroll down
+		//Scroll down
 		JavascriptExecutor scrollDown = (JavascriptExecutor)driver;
 		scrollDown.executeScript("window.scrollBy(0,600)");
 		
-	//Verify Password strength indicator
-	//usual sendKeys will not trigger the password-meter [driver.findElement(By.id("password")).sendKeys("1234abcd")]
+		//Verify Password strength indicator
+		//driver.findElement(By.id("password")).sendKeys("1234abcd");
 		WebElement weakPassword = driver.findElement(By.id("password"));
 		new Actions(driver).sendKeys(weakPassword, "1234abcd").perform();		
 		String passwordMeter = driver.findElement(By.id("password-strength-meter-container")).getAttribute("class");
@@ -98,7 +68,7 @@ public class CreateAccount extends Driver{
 		assertEquals(passwordMeter, "password-very-strong");
 		
 		
-	//Verify Confirm Password
+		//Verify Confirm Password
 		driver.findElement(By.id("password-confirmation")).sendKeys("Password1");
 		driver.findElement(By.xpath("//div[@class=\"primary\"]/button[@type=\"submit\"]")).click();
 		String passwordConfirm = driver.findElement(By.id("password-confirmation-error")).getText();
