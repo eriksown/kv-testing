ace3b374496f2261e8608b9146995de6669ef8cd
eriksown <kierson.vigilla@gmail.com>
2023-10-02 18:58:14 -0400

    Update UserLogin.java
    Updated passkey to get from CreateAccount.java

--- demoTest1/src/test/java/demoTest1/UserLogin.java
+++ demoTest1/src/test/java/demoTest1/UserLogin.java
@@ -15,14 +15,14 @@ public class UserLogin extends Driver{
 	WebDriver driver = Driver.driver;
 	
 	String userEmail = CreateAccount.email;
-	String passkey = "Password123!!!";
+	String passkey = CreateAccount.passkey;
 	String fName = CreateAccount.fName;
 	String lName = CreateAccount.lName;
 	
 	@Test (groups = "mainRun")
-	public void login() {
+	public void login() throws InterruptedException {
 		
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		
 		try {
 			driver.get("https://magento.softwaretestingboard.com/");
@@ -34,18 +34,19 @@ public class UserLogin extends Driver{
 		driver.findElement(By.partialLinkText("Sign In")).click();
 		driver.findElement(By.id("email")).sendKeys(userEmail);
 		driver.findElement(By.id("pass")).sendKeys(passkey);
+		Thread.sleep(2000);
 		driver.findElement(By.id("send2")).click();
 		
-		WebDriverWait waitAccountPage = new WebDriverWait(driver, Duration.ofSeconds(2));
+		WebDriverWait waitAccountPage = new WebDriverWait(driver, Duration.ofSeconds(3));
 		waitAccountPage.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class=\"logged-in\"]")));
 	
 	//Verify page and welcome message
 		String welcomeTitle = driver.getTitle();
 		assertEquals(welcomeTitle, "Home Page");
-		
+		Thread.sleep(2000);
 		String loginGreeting = driver.findElement(By.xpath("//span[@class=\"logged-in\"]")).getText();
 		assertEquals(loginGreeting, "Welcome, "+fName+" "+lName+"!");
 
-		driver.quit();
+		//driver.quit();
 	}
 }
